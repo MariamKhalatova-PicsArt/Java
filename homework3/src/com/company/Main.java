de4ec1839a14d549639aca7c9c2cda72b11fa7f1
Mariam <mar.khalatova@gmail.com>
2019-01-27 23:19:53 +0400

    Add my third homework

---
+++ homework3/src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void ma in(String[] args) {
+        Print1_100( );
+    }
+    public static void Print1_100(){
+         int   a = 1;
+        while(a < 101){
+            System.out.println(a);
+            a ++ ;
+
+        }
+    }
+
+}
